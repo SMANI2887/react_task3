f47a4f1223cc3addc842b802e2391735cfb0ef4c
SMANI2887 <smani2887@gmail.com>
2024-02-19 13:41:34 +0530

    check final

--- src/components/pages/Page_Error.jsx
+++ src/components/pages/Page_Error.jsx
@@ -9,7 +9,7 @@ function Page_Error() {
   <div className="error mx-auto" data-text={404}>404</div>
   <p className="lead text-gray-800 mb-5">Page Not Found</p>
   <p className="text-gray-500 mb-0">It looks like you found a glitch in the matrix...</p>
-  <a href="index">← Back to Dashboard</a>
+  <a href="/">← Back to Dashboard</a>
 </div>
 {/* /.container-fluid */}
 {/* End of Main Content */}
